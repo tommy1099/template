7bc7ac58a8ddc058afdc8f2a09c821f7cfaef296
tommy1099 <amir.agness.aa@gmail.com>
2023-09-12 07:28:22 +0330

    Revert "just a template for practicing"
    This reverts commit 0a9583200af83b48ca45c72361fbf631b26c17cb.

--- model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema({
-  src: {
-    type: String,
-    require: true,
-  },
-});
-
-module.exports = mongoose.model("DOGDB", Schema);
